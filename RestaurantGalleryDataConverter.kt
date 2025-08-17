0d71e4926b78944796993aa633fc6cbb33a5d540
sryang <sarang628@gmail.com>
2025-08-18 07:36:15 +0800

    update repository

--- RestaurantGalleryDataConverter.kt
+++ RestaurantGalleryDataConverter.kt
@@ -3,8 +3,9 @@ package com.sarang.torang.di.restaurant_gallery_di
 import com.sarang.torang.BuildConfig
 import com.sarang.torang.data.RestaurantDetail
 import com.sarang.torang.data.RestaurantImage
+import com.sarang.torang.data.remote.response.RestaurantDetailApiModel
 
-fun RestaurantDetail.toRestaurantImages(): List<RestaurantImage> {
+fun RestaurantDetailApiModel.toRestaurantImages(): List<RestaurantImage> {
     return this.pictures.map {
         RestaurantImage(
             id = it.picture_id,
